b6991faf07b923bf3fa3f0d18fa9904aa6b3719d
Erwin de Keijzer <erwin@dekeijzer.xyz>
2024-03-26 13:58:59 +0100

    Removed more dead code and made it loop

--- main.go
+++ main.go
@@ -3,37 +3,37 @@ package main
 import (
 	"log/slog"
 	"time"
-
-	"github.com/kelseyhightower/envconfig"
 )
-
-type config struct {
-	LeaseTime           time.Duration `default:"10m"`
-	Database            string        `default:"./dhpresence.db"`
-	Webhook             string
-	ReconnectAcceptList []string `default:""`
-	NewBlockList        []string `default:""`
-}
 
 func main() {
 
-	var cfg config
-	err := envconfig.Process("dh", &cfg)
-	if err != nil {
-		slog.Error("Failed to parse env", "error", err)
-		return
-	}
+	activeDevices := make(map[string]Device)
+	firstRun := true
 
 	slog.Info("Starting dhcpresence")
-	devices, err := getDevices()
-	if err != nil {
-		slog.Error("Failed to get cookie", "error", err)
-		return
-	}
 
-	for _, device := range devices {
-		if !device.Active {
-			slog.Info("Device found", "device", device.Name, "active", device.Active, "lastSeen", device.LastConnection.In(time.Local))
+	for {
+		slog.Info("Getting devices")
+		devices, err := getDevices()
+		if err != nil {
+			slog.Error("Failed to get cookie", "error", err)
+			return
 		}
+
+		for _, device := range devices {
+			_, wasActive := activeDevices[device.Key]
+			if device.Active {
+				if !wasActive && !firstRun {
+					slog.Info("Device connected", "device", device.Name, "active", device.Active, "lastSeen", device.LastConnection.In(time.Local))
+				}
+				activeDevices[device.Key] = device
+			} else if wasActive && !firstRun {
+				slog.Info("Device disconnected", "device", device.Name, "active", device.Active, "lastSeen", device.LastConnection.In(time.Local))
+				delete(activeDevices, device.Key)
+			}
+		}
+		firstRun = false
+		slog.Info("got devices", "devices", len(devices), "active", len(activeDevices))
+		time.Sleep(30 * time.Second)
 	}
 }
